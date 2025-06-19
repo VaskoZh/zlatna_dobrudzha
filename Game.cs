aa081869f9f11c3fda7d79e57875d84930473192
studentsVisualStudio@itpg-varna.bg <studentsVisualStudio@itpg-varna.bg>
2025-06-19 13:59:18 +0300

    first commit

---
+++ Game.cs
@@ -0,0 +1,10 @@
+﻿namespace zlatna_dobrudzha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
